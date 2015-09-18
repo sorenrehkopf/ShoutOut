abe411eb1f159c0cc81322a33de9d2f9a7d38785
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-09-17 18:09:25 -0700

    socket connection working for posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var Post = require('../models/post.js');
 var Comment = require('../models/comment.js');
 
+// var socket = io();
+
 //allow api requests from ionic front end
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,15 +13,19 @@ router.use(function(req, res, next) {
 });
 
 router.get('/:lon/:lat',function(req,res,next){
-	var latMin = parseFloat(req.params.lat)-.0002;
-	var latMax = parseFloat(req.params.lat)+.0002;
-	var lonMin = parseFloat(req.params.lon)-.0002;
-	var lonMax = parseFloat(req.params.lon)+.0002;
+	var latMin = parseFloat(req.params.lat)-.002;
+	var latMax = parseFloat(req.params.lat)+.002;
+	var lonMin = parseFloat(req.params.lon)-.002;
+	var lonMax = parseFloat(req.params.lon)+.002;
 	console.log(latMin,lonMin)
-	Post.find({'location.lat':{$gte:latMin,$lte:latMax},'location.lon':{$gte:lonMin,$lte:lonMax}},function(err,posts){
+	Post.find({'location.lat':{$gte:latMin,$lte:latMax},'location.lon':{$gte:lonMin,$lte:lonMax}}).
+	skip().
+	limit(10).
+	sort({date:-1}).
+	exec(function(err,posts){
 		res.send(posts)
 	})
-});
+});	
 
 router.post('/',function(req,res,next){
 	var post = new Post();
@@ -34,6 +40,9 @@ router.post('/',function(req,res,next){
 
 router.get('/:id',function(req,res,next){
 	Post.findOne({_id:req.params.id},function(err,post){
+		post.comments = post.comments.sort(function(comment1,comment2){
+			return comment2.date - comment1.date
+		})
 		res.send(post);
 	})
 });
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var io = require('socket.io')
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/shoutout_dev';
 mongoose.connect(uristring, function(err, res){
 	if (err) {
@@ -8,11 +9,22 @@ mongoose.connect(uristring, function(err, res){
       console.log ('Succeeded connected to: ' + uristring);
   	}
 });
+var Post = require('./models/post.js');
+var Comment = require('./models/comment.js');
 var bodyParser = require('body-parser');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+
+	socket.on('new post', function(data){
+			io.emit('new post');
+		});
+})
 
 //App config
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/posts', require('./controllers/posts.js'));
 
-app.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000);
